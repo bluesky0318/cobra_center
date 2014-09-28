34a96bb1a4103b74d0c5d8719dea5ff099f6943c
bluesky0318 <42303596@qq.com>
2014-09-28 16:19:07 +0800

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  
+  root  to: 'pages#home'
+
+  match '/help',    to: 'pages#help',           via: 'get'
+  match '/about',   to: 'pages#about',          via: 'get'
+  match '/contact', to: 'pages#contact',        via: 'get'
+  match '/news',    to: 'pages#news',           via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
